d2fc6163735ea0e5ba7b9a3f99e91fdbfce56769
FuriousGopher <alkazavr94@gmail.com>
2022-02-02 11:44:54 +0100

    01

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 document.body.innerHTML = '<h1>hello from javaScript</h1>'
+  //  alert(1)
+   // const firstName = 'Ivan'
+  //  const lastname = 'Kabar'
+   // const age = 26
+   // const  isProgrammer = true
+
+    //const _private = 'private'
+    //const $ = 'some value'
+ //   const if = 'makd' // err
+   // const witchNum5 = '5'
+    // const 5witchNum = 5 // err
+
+    // 2 Мутир
+ // alert ('Name: ' +firstName + ' age : ' +age)
+  //  let currentYear = 2022
+   // const birthYear = 1994
+
+    //const age = currentYear  birthYear
+   // const a = 10
+   // const  b = 5
+//let c = 32
+ //   c = c+a
+ //   console.log((a+b))
+  //  console.log (a-b)
+  //  console.log(age)
+   // console.log (currentYear++)
+   // console.log (currentYear)
+  //  console.log(c)
+ //   const isProgrammer = true
+//  const name = 'Ivan'
+ // const age = 27
+ // console.log(typeof isProgrammer)
+ // console.log(typeof name)
+ // console.log(typeof age)
+
+ // const fullAge = 26// const birthYear = 1994
+  //const currentYear = 2022
+
+ // const isfullAge = (currentYear - birthYear)  >= fullAge
+ // console.log(isfullAge)
