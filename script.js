bc4683ef48152ab467ff254f7e694c0f66ad88d5
FuriousGopher <alkazavr94@gmail.com>
2022-02-02 12:23:36 +0100

    02

--- script.js
+++ script.js
@@ -39,3 +39,43 @@ document.body.innerHTML = '<h1>hello from javaScript</h1>'
 
  // const isfullAge = (currentYear - birthYear)  >= fullAge
  // console.log(isfullAge)
+ // Operatori
+  //const courseStatus = 'fail' // ready fail pending
+
+ // if (courseStatus === 'ready') {
+     // console.log('kurs ready')
+ // } else if (courseStatus === 'pending') {
+    //  console.log('kurs not reaady')
+  //} else {
+    //  console.log('kurs not ready')
+ // }
+ // const isReady = true
+
+ // if (isReady) {
+  //    console.log('vse ready')
+ // } else{
+  //    console.log('vse ne ready')
+ // }
+  // Ternar
+ // isReady ? console.log('Ves Ready') : console.log('vse ne ready')
+
+ // const num1 = 42
+ // const num2 = '42'
+ // console.log(num1==num2)
+
+  // Bull logik
+
+  // Function
+
+  function calculateAge(year) {
+   return 2020 - year
+ }
+
+// const  myAge = calculateAge(1994)
+//console.log(myAge)
+ function logInfoAbout(name, Year) {
+   const age = calculateAge(year)
+     console.log('Pers name ' + name +  'age' + age)
+ }
+
+ logInfoAbout( name: 'Ivan', year:1994)
